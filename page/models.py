fd258ea27e8546e616b63bec62b12aee79b5fb11
qkrghd147 <qkrghd147@likelion.org>
2020-08-10 22:07:45 +0900

    third commit

--- page/models.py
+++ page/models.py
@@ -7,3 +7,6 @@ class Designer(models.Model):
     name = models.CharField(max_length = 50)
     address = models.CharField(max_length = 255)
     description = models.TextField()
+
+    def __str__(self):
+        return self .name
